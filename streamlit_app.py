6e6de67b93108a3c8b37ab9269f92d2ea1a70e1c
MaminirinaAndr1 <117756329+MaminirinaAndr1@users.noreply.github.com>
2025-06-02 13:36:52 +0000

    Add Select box

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,3 +87,11 @@ chart_data = pd.DataFrame(np.random.randn(20, 3),
                           )
 
 st.line_chart(chart_data)
+
+# Select Box
+st.header('st.selectbox')
+
+option = st.selectbox(What is your favorite color?,
+                      ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
