c0ec6fbe9004a1e21367ed88a7db394c9ee27f84
MaminirinaAndr1 <117756329+MaminirinaAndr1@users.noreply.github.com>
2025-06-02 13:46:24 +0000

    Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,3 +95,14 @@ option = st.selectbox('What is your favorite color?',
                       ('Blue', 'Red', 'Green'))
 
 st.write('Your favorite color is ', option)
+
+# Multiselect
+st.header('st.multiselect')
+
+options = st.multiselect(
+    'What are your favorite colors?',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red']
+)
+
+st.write('You selected:', options)
